5ffe94c488f9e24928d74c53d495d2859b7d1e78
ManuelDev <manueldeveloper17@gmail.com>
2022-09-22 01:21:11 -0500

    Initial styles

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,23 +1,66 @@
 import React from "react";
+import Link from "next/link";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 // Components
 import Buscar from "../ui/Buscar";
+import Navegacion from "./Navegacion";
+
+// Styled components
+const ContenedorHeader = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  // Media query, se aplica de 768px hacia arriba
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.p`
+  color: var(--verde);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: "Roboto Slab", serif;
+  margin-right: 2rem;
+`;
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    // Aplicando styles props
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
+    >
+      <ContenedorHeader>
         <div>
-          <p>P</p>
+          <Link href="/">
+            <Logo>P</Logo>
+          </Link>
 
           {/* Buscador */}
           <Buscar />
 
           {/* Navegacion */}
+          <Navegacion />
         </div>
 
-        <div>{/* Menu de administracion */}</div>
-      </div>
+        <div>
+          {/* Menu de administracion */}
+          <p>Hola: Manuel</p>
+
+          <button type="button">Cerrar Sesion</button>
+
+          <Link href="/">Login</Link>
+          <Link href="/">Crear Cuenta</Link>
+        </div>
+      </ContenedorHeader>
     </header>
   );
 };
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Link from "next/link";
+import { Global, css } from "@emotion/react";
+import Head from "next/head";
 
 // Components
 import Header from "./Header";
@@ -7,6 +8,84 @@ import Header from "./Header";
 const Layout = (props) => {
   return (
     <>
+      {/* Estilos globales de la aplicacion  */}
+      <Global
+        styles={css`
+          :root {
+            --gris: #3d3d3d;
+            --gris2: #6f6f6f;
+            --gris3: #e1e1e1;
+            --verde: #26e2bf;
+          }
+
+          html: {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+
+          body {
+            font-size: 1.4rem; /* 14 px, gracias a la linea del font-size: 62.5% */
+            line-height: 1.5;
+            font-family: "PT Sans", sans-serif;
+          }
+
+          h1,
+          h2,
+          h3 {
+            margin: 0 0 2rem 0;
+            line-height: 1.5;
+          }
+
+          h1,
+          h2 {
+            font-family: "Roboto Slab", serif;
+            font-weight: 700;
+          }
+
+          h3 {
+            font-family: "PT Sans", sans-serif;
+          }
+
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+
+          a {
+            text-decoration: none;
+          }
+        `}
+      />
+
+      {/* Head HTML, aca van fuentes, meta etc */}
+      <Head>
+        <html lang="es" />
+        <title>We Products Firebase y Next.js</title>
+        {/* Normalize para quitar estilos del navegador*/}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* Importando CSS */}
+        <link href="/static/css/app.css" rel="stylesheet" />
+      </Head>
+
       <Header />
 
       <main>{props.children}</main>
